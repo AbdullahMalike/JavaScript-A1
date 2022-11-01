dba23f91a03e109b49000b663674d2b64838992d
AbdullahMalike <abdullahmalikae2002@gmail.com>
2022-11-01 19:26:20 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,195 @@
+// chapter 1
+
+alert("Thanks for your input!");
+window.alert("Abdullah");
+
+// chapter 2
+
+var name="Mark";
+alert(name);
+
+var name ="Ace";
+alert(name);
+
+var nationality="U.S.";
+alert(nationality);
+
+var floogle = "Mark";
+alert(floogle);
+
+var lessonAuthor = "Mark";
+alert(lessonAuthor);
+
+
+var guyWhoKeepsSayingHisOwnName = "Mark";
+alert(guyWhoKeepsSayingHisOwnName);
+
+
+var x = "Mark";
+alert(x);
+
+var lastName = "Smith";
+alert(lastName);
+
+var cityOfOrigin = "New Orleans";
+alert(cityOfOrigin);
+
+var aussieGreeting = "g'Day";
+alert(aussieGreeting);
+
+var nickname = "Bub";
+alert(nickname);
+
+alert("Thanks for your input!");
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+// chapter 3
+
+var weight = 150+25;
+alert(weight);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum);
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+alert(originalNum);
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+alert(newNum);
+
+//var 1stPresident = "Washington"; illegal declaration.
+
+var prezWhoCame1st = "Washington"; //legal declaration.
+alert(prezWhoCame1st);
+
+alert(144);
+
+var caseQty = 144;
+alert(caseQty);
+
+// chapter 4
+
+/* legal and illegal var name 
+ 1name illegal
+ name1 legal
+ first name illegal
+ first_name legal
+ name@ ilegal
+*/
+
+// chapter 5
+
+
+var popularNumber = 4;
+alert(popularNumber);
+
+var popularNumber = 2 + 2;
+alert(popularNumber);
+
+alert(2 + 2);
+
+var popularNumber = 12 - 24;
+alert(popularNumber);
+
+var popularNumber = 3 * 12;
+alert(popularNumber);
+
+var num = 10;
+var popularNumber = num + 200;
+alert(popularNumber);
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+alert(popularNumber);
+
+var whatsLeftOver = 10 % 3;
+alert(whatsLeftOver);
+
+var whatsLeftOver = 9 % 3;
+alert(whatsLeftOver);
+
+// chapter 6
+
+var num=10;
+num = num + 1;
+num++;
+num--;
+alert(num);
+
+var num = 1;
+var newNum = num++;
+alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+alert(newNum);
+
+var num = 1;
+var newNum = num--;
+alert(newNum);
+
+var num = 1;
+var newNum = --num;
+alert(newNum);
+
+
+// chapter 7
+
+
+var totalCost = 1 + 3 * 4;
+alert(totalCost);
+
+var totalCost = 1 + (3 * 4);
+alert(totalCost);
+
+var totalCost = (1 + 3) * 4;
+alert(totalCost);
+
+var resultOfComputation = (2 * 4) * 4 + 2;
+alert(resultOfComputation);
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+alert(resultOfComputation);
+
+resultOfComputation = (2 * 4) * (4 + 2);
+alert(resultOfComputation);
+
+// chapter 8
+
+var message = "Thanks for your input!";
+alert(message);
+
+var userName= "Abdullah";
+alert("Thanks, " + userName + "!");
+
+var message = "Thanks, ";
+userName = "Abdullah";
+var banger = "!";
+alert(message + userName + banger);
+
+alert("Thanks, " + "Abdullah" + "!");
+
+
+var message = "Thanks, ";
+var userName = "Abdullah";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+
+
+alert("2" + "2");
+
+
+alert("2 plus 2 equals " + 2 + 2);
